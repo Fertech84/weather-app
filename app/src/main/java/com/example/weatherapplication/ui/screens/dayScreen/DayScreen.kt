765e42d75a51c4c0ef12a0b44afeabb7f523111b
Juan Fernando Martinez Dominguez <juan.md@globant.com>
2024-11-19 16:38:23 -0500

    weather main component created

---
+++ app/src/main/java/com/example/weatherapplication/ui/screens/dayScreen/DayScreen.kt
@@ -0,0 +1,53 @@
+package com.example.weatherapplication.ui.screens.dayScreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.weatherapplication.ui.components.WeatherMainComponent
+
+@Composable
+fun DayScreen() {
+    Scaffold(
+        topBar = {DayScreenToolBar()}
+    ) { innerPadding ->
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(innerPadding)
+                .fillMaxWidth()
+        ) {
+            items(5) {
+                WeatherMainComponent()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DayScreenToolBar() {
+    TopAppBar(
+        title = {
+            Text(text = "Name")
+        },
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "")
+            }
+        },
+
+        )
+}
